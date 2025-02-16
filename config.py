05f84433c7f8a7ef89d4f86ca6c105d8f84dba1e
Muhammad Al-Ashmawy <muhammad.al_ashmawy@outlook.com>
2025-02-16 21:59:17 +0200

    correct connections are now made ; )

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 # MongoDB Configuration
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://ashmawy:<db_password>@my-free-cluster.0am2f.mongodb.net/")
 DB_NAME = "news_aggregator"
 COLLECTION_NAME = "headlines"
