fa560cc8adf88820d200a854aa743ae8e3f25d9f
Muhammad Al-Ashmawy <muhammad.al_ashmawy@outlook.com>
2025-02-26 16:53:24 +0200

    better error handling ; )

--- the-market-collector/app/models.py
+++ the-market-collector/app/models.py
@@ -22,29 +22,31 @@ def fetch_stocks():
     Fetches stock data from Alpha Vantage API.
     """
     api_key = ALPHA_VANTAGE_API_KEY
-    symbol = "TSLA"  # Add more stock symbols as needed
+    symbol = "TSLA"
     all_stocks_data = []
 
-    api_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey={api_key}"
+    api_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval=5min&apikey={api_key}"
     response = requests.get(api_url)
     
     if response.status_code == 200:
         data = response.json()
-        print(f"✅ Successfully fetched data for {symbol}.", data)
-        for timestamp, stock_data in data.items():
-            stock_record = {
-                "symbol": symbol,
-                "timestamp": timestamp,
-                "open": stock_data["1. open"],
-                "high": stock_data["2. high"],
-                "low": stock_data["3. low"],
-                "close": stock_data["4. close"],
-                "volume": stock_data["5. volume"]
-            }
-            all_stocks_data.append(stock_record)
+        if not data:
+            print(f"❌ No data fetched for {symbol}.")
+        else:
+            print(f"✅ Successfully fetched data for {symbol}.", data)
+            for timestamp, stock_data in data.items():
+                stock_record = {
+                    "symbol": symbol,
+                    "timestamp": timestamp,
+                    "open": stock_data["1. open"],
+                    "high": stock_data["2. high"],
+                    "low": stock_data["3. low"],
+                    "close": stock_data["4. close"],
+                    "volume": stock_data["5. volume"]
+                }
+                all_stocks_data.append(stock_record)
     else:
         print(f"❌ Failed to fetch data for {symbol}. Status code: {response.status_code}")
-        
 
     if not all_stocks_data:
         print(f"❌ No stock data fetched. Status Code: {response.status_code}")
