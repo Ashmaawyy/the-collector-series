c27b61b64d76402bea0374a3e27b67d7dca613ed
Muhammad Al-Ashmawy <muhammad.al_ashmawy@outlook.com>
2025-03-14 16:25:27 +0200

    fixed a typo ; )

--- the-market-collector/app/app.py
+++ the-market-collector/app/app.py
@@ -37,4 +37,4 @@ if __name__ == "__main__":
     store_stocks_job()
     
     logger.info("🚀 The Market Collector starting on port 5000")
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=True, port=5000, host="0.0.0.0")
